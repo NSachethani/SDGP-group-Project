56feeccaa2cc0fbc6e0529fc5da409973b590fb3
Wolf Wizard <ebbhanuka@gmail.com>
2025-02-28 22:57:32 +0530

    Added app-timer and usage-limits files to implement those features.
    - Added app-timer.tsx and usage-limits.tsx files to start implementation
    
    - Changed _layout files to hide headers for best UI experience.

---
+++ Front_End/app/(root)/(usage-timer)/app-timer.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const apptimer = () => {
+  return (
+    <View>
+      <Text>apptimer</Text>
+    </View>
+  )
+}
+
+export default apptimer
---
+++ Front_End/app/(root)/(usage-timer)/usage-limits.tsx
@@ -0,0 +1,40 @@
+import { View, Text, ImageBackground,BackHandler, } from "react-native";
+import React, { useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+
+const UsageLimits = () => {
+
+// Back button to go to home screen (Hardware back button)
+useEffect(() => {
+  const backAction = () => {
+    router.replace("/(root)/(tabs)/home");
+    return true;
+  };
+  BackHandler.addEventListener("hardwareBackPress", backAction);
+  return () =>
+    BackHandler.removeEventListener("hardwareBackPress", backAction);
+}, []);
+
+
+  return (
+    <>
+      <ImageBackground
+        source={require("@/assets/images/backImg.jpg")}
+        style={{ flex: 1 }}
+      >
+        <SafeAreaView className="flex-1">
+          <Text className="text-white font-bold">
+            Start Cording in here and don't remove safeareaview and
+            imagebackground
+          </Text>
+          <View className="flex-1 items-center justify-center">
+            <Text className="text-xl mb-6">Usage Limits</Text>
+          </View>
+        </SafeAreaView>
+      </ImageBackground>
+    </>
+  );
+};
+
+export default UsageLimits;
